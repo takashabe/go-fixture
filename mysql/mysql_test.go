50577b1a1532c2a2789731f832b71164c811ff60
takashabe <takashabe.public@gmail.com>
2018-03-10 17:35:40 +0900

    Fix mysql_test

--- mysql/mysql_test.go
+++ mysql/mysql_test.go
@@ -2,17 +2,30 @@ package mysql
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
 	"testing"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func testDB(t *testing.T) *sql.DB {
-	db, err := sql.Open("mysql", "db_fixture@/db_fixture")
-	if err != nil {
-		t.Fatal("failed to open db")
+func connectDB() (*sql.DB, error) {
+	name := getEnvWithDefault("DB_NAME", "db_fixture")
+	host := getEnvWithDefault("DB_HOST", "127.0.0.1")
+	port := getEnvWithDefault("DB_PORT", "3306")
+	user := getEnvWithDefault("DB_USER", "root")
+	pass := getEnvWithDefault("DB_PASSWORD", "")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, pass, host, port, name)
+
+	return sql.Open("mysql", dsn)
+}
+
+func getEnvWithDefault(name, def string) string {
+	env := os.Getenv(name)
+	if len(env) != 0 {
+		return env
 	}
-	return db
+	return def
 }
 
 func TestTrimComment(t *testing.T) {
@@ -71,6 +84,11 @@ func TestEscapeValue(t *testing.T) {
 }
 
 func TestExecSQL(t *testing.T) {
+	db, err := connectDB()
+	if err != nil {
+		t.Fatalf("want non error, got %v", err)
+	}
+
 	cases := []struct {
 		input string
 	}{
@@ -87,7 +105,6 @@ func TestExecSQL(t *testing.T) {
 		},
 	}
 	for i, c := range cases {
-		db := testDB(t)
 		tx, err := db.Begin()
 		if err != nil {
 			t.Fatalf("#%d: want non error, got %v", i, err)
